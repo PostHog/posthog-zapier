8a2d92ba42639cc3484305d8bed584b85360895b
Twixes <dev@twixes.com>
2020-07-23 00:53:30 +0200

    Add basic samples

--- src/creates/event_capture.ts
+++ src/creates/event_capture.ts
@@ -2,44 +2,44 @@ import { Bundle, ZObject } from 'zapier-platform-core'
 import { composeAPIURL } from '../utils'
 
 interface InputData {
-  event_name: string
-  user_distinct_id: string
-  timestamp: string
-  [property: string]: any
+    event_name: string
+    user_distinct_id: string
+    timestamp: string
+    [property: string]: any
 }
 
 async function perform(z: ZObject, bundle: Bundle<InputData>) {
-  const response = await z.request({
-    method: 'POST',
-    url: composeAPIURL('capture'),
-    body: {
-      event: bundle.inputData.event_name,
-      properties: {
-          distinct_id: bundle.inputData.user_distinct_id,
-      },
-      timestamp: bundle.inputData.timestamp
-    },
-  });
-  return response.data;
-};
+    const response = await z.request({
+        method: 'POST',
+        url: composeAPIURL('capture'),
+        body: {
+            event: bundle.inputData.event_name,
+            properties: {
+                distinct_id: bundle.inputData.user_distinct_id,
+            },
+            timestamp: bundle.inputData.timestamp,
+        },
+    })
+    return response.data
+}
 
 export const EventCaptureCreate = {
-  key: 'event_capture',
-  noun: 'Event',
+    key: 'event_capture',
+    noun: 'Event',
 
-  display: {
-    label: 'Capture Event',
-    description: 'Captures an event.',
-  },
-
-  operation: {
-    perform,
-    inputFields: [
-      { key: 'title', required: true },
-      { key: 'year', type: 'integer' },
-    ],
-    sample: {
-      // fields
+    display: {
+        label: 'Capture Event',
+        description: 'Captures an event.',
     },
-  },
-};
+
+    operation: {
+        perform,
+        inputFields: [
+            { key: 'title', required: true },
+            { key: 'year', type: 'integer' },
+        ],
+        sample: {
+            id: 1,
+        },
+    },
+}
--- src/triggers/action_performed.ts
+++ src/triggers/action_performed.ts
@@ -1,24 +1,24 @@
-import { Bundle, ZObject } from 'zapier-platform-core';
+import { Bundle, ZObject } from 'zapier-platform-core'
 import { composeAPIURL } from '../utils'
 
 const perform = async (z: ZObject, bundle: Bundle) => {
-  const response = await z.request(composeAPIURL('hook/action_performed'))
-  return response.data;
-};
+    const response = await z.request(composeAPIURL('hook/action_performed'))
+    return response.data
+}
 
 export const ActionPerformedTrigger = {
-  key: 'action_performed',
-  noun: 'Action',
+    key: 'action_performed',
+    noun: 'Action',
 
-  display: {
-    label: 'Action Performed',
-    description: 'Triggers when an action is performed by a user.',
-  },
-
-  operation: {
-    perform,
-    sample: {
-      // fields
+    display: {
+        label: 'Action Performed',
+        description: 'Triggers when an action is performed by a user.',
     },
-  },
-};
+
+    operation: {
+        perform,
+        sample: {
+            id: 1,
+        },
+    },
+}
