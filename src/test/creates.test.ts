46dc5e38938a77f8509de69a4841576262657f36
Twixes <dev@twixes.com>
2020-07-23 00:10:53 +0200

    Update tests

--- src/test/creates.test.ts
+++ src/test/creates.test.ts
@@ -1,21 +1,21 @@
 /* globals describe, expect, test */
 
-import { createAppTester, tools } from 'zapier-platform-core';
+import { createAppTester, tools } from 'zapier-platform-core'
 
-import App from '../index';
+import App from '../index'
 
-const appTester = createAppTester(App);
-tools.env.inject();
+const appTester = createAppTester(App)
+tools.env.inject()
 
-/*
 describe('event_capture', () => {
-  test('capture event', async () => {
+    test('capture event', async () => {
+        /*
     const bundle = { inputData: { title: 'hello', year: 2020 } };
     const result = await appTester(App.creates.action_performed.operation.perform, bundle);
     expect(result).toMatchObject({
       title: 'hello',
       year: 2020,
     });
-  });
-});
-*/
+    */
+    })
+})
--- src/test/triggers.test.ts
+++ src/test/triggers.test.ts
@@ -1,21 +1,20 @@
 /* globals describe, expect, test */
 
-import { Bundle, createAppTester, tools } from 'zapier-platform-core';
+import { Bundle, createAppTester, tools } from 'zapier-platform-core'
 
-import App from '../index';
+import App from '../index'
 
-const appTester = createAppTester(App);
-tools.env.inject();
+const appTester = createAppTester(App)
+tools.env.inject()
 
-/*
 describe('action_performed', () => {
-  test('list actions', async () => {
-    const bundle = { inputData: {} };
+    test('list actions', async () => {
+        /*const bundle = { inputData: {} };
     const results = (await appTester(
       App.triggers.action_performed.operation.perform,
       bundle
     )) as Array<object>;
     expect(results.length).toBeGreaterThan(0);
-  });
-});
-*/
+    */
+    })
+})
