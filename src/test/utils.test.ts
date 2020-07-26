17845bd873b14222ea5d62c3aa5eff30c59383c4
Twixes <dev@twixes.com>
2020-07-26 04:36:01 +0200

    Update utils

--- src/test/utils.test.ts
+++ src/test/utils.test.ts
@@ -5,11 +5,11 @@ import { composeURL, composeAPIURL } from '../utils'
 describe('composeURL', () => {
     test('compose URL 1', () => {
         const composedURL: string = composeURL('/setup', 'https://app.posthog.com/')
-        expect(composedURL).toEqual('https://app.posthog.com/setup')
+        expect(composedURL).toEqual('https://app.posthog.com/setup/')
     })
     test('compose URL 2', () => {
-        const composedURL: string = composeURL('foo', 'https://company.com/posthog')
-        expect(composedURL).toEqual('https://company.com/posthog/foo')
+        const composedURL: string = composeURL('foo', 'https://company.com/posthog/')
+        expect(composedURL).toEqual('https://company.com/posthog/foo/')
     })
 })
 
@@ -20,6 +20,6 @@ describe('composeAPIURL', () => {
     })
     test('compose API URL 2', () => {
         const composedAPIURL: string = composeAPIURL('/bar', 'https://company.com/posthog')
-        expect(composedAPIURL).toEqual('https://company.com/posthog/api/bar')
+        expect(composedAPIURL).toEqual('https://company.com/posthog/api/bar/')
     })
 })
--- src/utils.ts
+++ src/utils.ts
@@ -1,9 +1,9 @@
 import { API_HOST } from './settings'
 
 export function composeURL(path: string, host: string = API_HOST): string {
-    return `${host.replace(/[/]+$/, '')}/${path.replace(/^[/]+/, '')}`
+    return `${host.replace(/[/]+$/, '')}/${path.replace(/(^[/]+)|([/]+$)/, '')}/`
 }
 
 export function composeAPIURL(path: string, host: string = API_HOST): string {
-    return composeURL(`api/${path.replace(/^[/]+/, '')}`, host)
+    return composeURL(`api/${path.replace(/(^[/]+)|([/]+$)/, '')}/`, host)
 }
