0f5af0a342d40c84d5dc468bd04d1a2c1a5be74a
Twixes <dev@twixes.com>
2020-07-26 04:36:23 +0200

    Make authentication proper

--- src/authentication.ts
+++ src/authentication.ts
@@ -1,22 +1,27 @@
 import { Bundle, ZObject } from 'zapier-platform-core'
-import { composeAPIURL } from './utils'
+import { composeURL, composeAPIURL } from './utils'
 
-function test(z: ZObject, bundle: Bundle) {
-    return z
-        .request({
-            url: composeAPIURL('user'),
-        })
-        .then((response) => {
-            if (response.status === 401) {
-                throw new Error('The API Key you supplied is invalid')
-            }
-            return response.json
-        })
+async function test(z: ZObject, bundle: Bundle) {
+    const response = await z.request({
+        url: composeAPIURL('dashboard'),
+    })
+    if (response.status === 401 || response.status === 403) {
+        throw new Error('The personal access token you supplied is invalid')
+    }
+    return response.json
 }
 
 export const authentication = {
     type: 'custom',
-    fields: [{ key: 'apiKey', label: 'Read-write API key', required: true, type: 'string' }],
+    fields: [
+        {
+            key: 'api_key',
+            label: 'Personal Access Token',
+            helpText: `Get your token the [Setup page](${composeURL('setup')}).`,
+            required: true,
+            type: 'string',
+        },
+    ],
     test,
     connectionLabel: (z: ZObject, bundle: Bundle) => {
         return bundle.inputData.username
