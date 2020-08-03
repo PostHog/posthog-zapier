29f40dea7b1eff2f354733da3828636dad3a93b9
Twixes <dev@twixes.com>
2020-08-03 15:13:41 +0200

    Add AnnotationCreatedTrigger

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import { authentication, includeToken } from './authentication'
 import { EventCaptureCreate } from './creates/capture_event'
 import { ActionDefinedTrigger } from './triggers/action_defined'
 import { ActionPerformedTrigger } from './triggers/action_performed'
+import { AnnotationCreatedTrigger } from './triggers/annotation_created'
 
 const { version } = require('../package.json') // require() to bypass rootDir restriction
 
@@ -15,6 +16,7 @@ export default {
     triggers: {
         [ActionDefinedTrigger.key]: ActionDefinedTrigger,
         [ActionPerformedTrigger.key]: ActionPerformedTrigger,
+        [AnnotationCreatedTrigger.key]: AnnotationCreatedTrigger,
     },
     creates: {
         [EventCaptureCreate.key]: EventCaptureCreate,
---
+++ src/triggers/annotation_created.ts
@@ -0,0 +1,52 @@
+import { Bundle, ZObject } from 'zapier-platform-core'
+import { composeAPIURL, subscribeHookCreator, unsubscribeHook } from '../utils'
+
+function getAnnotation(z: ZObject, bundle: Bundle) {
+    return [bundle.cleanedRequest.data]
+}
+
+async function getFallbackRealAnnotation(z: ZObject, bundle: Bundle) {
+    const response = await z.request({
+        url: composeAPIURL('annotation'),
+    })
+    return (response.data as { results: object[] }).results
+}
+
+export const AnnotationCreatedTrigger = {
+    key: 'annotation_created',
+    noun: 'Annotation',
+
+    display: {
+        label: 'Annotation Created',
+        description: 'Triggers when an annotation is created by a team member.',
+    },
+
+    operation: {
+        inputFields: [],
+        type: 'hook',
+
+        performSubscribe: subscribeHookCreator('annotation_created'),
+        performUnsubscribe: unsubscribeHook,
+
+        perform: getAnnotation,
+        performList: getFallbackRealAnnotation,
+
+        sample: {
+            id: 99,
+            content: '2.0 Launch',
+            date_marker: '2077-11-11T12:34:56Z',
+            creation_type: 'USR',
+            dashboard_item: null,
+            created_by: {
+                id: 67,
+                distinct_id: 'NVsxdsUOiDn3AxVF5LguBWDlZBe8eZy-4rcefx16AIg',
+                first_name: 'Joe Dohn',
+                email: 'joe@example.com',
+            },
+            created_at: '2077-11-11T12:34:56Z',
+            updated_at: '2077-11-11T12:34:56Z',
+            deleted: false,
+            apply_all: true,
+        },
+    },
+}
