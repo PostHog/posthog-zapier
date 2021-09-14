7c09c5546be198b602dc99d3b423cabc8022c71c
Michael Matloka <dev@twixes.com>
2021-09-14 11:37:49 +0200

    Fix access to projects

--- src/triggers/project_created.ts
+++ src/triggers/project_created.ts
@@ -1,4 +1,5 @@
 import { Bundle, ZObject } from 'zapier-platform-core'
+import { UserType } from '../types'
 import { composeUrl } from '../utils'
 
 export const ProjectCreatedTrigger = {
@@ -16,7 +17,11 @@ export const ProjectCreatedTrigger = {
             const response = await z.request({
                 url: composeUrl(['api', 'users', '@me'], bundle),
             })
-            return (response.data as { teams: { id: number; name: string }[] }).teams
+            const organization = (response.data as UserType).organization
+            if (!organization) {
+                throw new Error("You don't belong to any PostHog organization! Create or join one.")
+            }
+            return organization.teams
         },
     },
 }
