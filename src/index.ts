bebb3e098e93151973bfc846dfcb9d2cffbbc67e
Michael Matloka <dev@twixes.com>
2022-08-26 18:28:48 +0200

    Remove unused triggers

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,7 @@ import { version as platformVersion } from 'zapier-platform-core'
 
 import { authentication, includeToken } from './authentication'
 import { EventCaptureCreate } from './creates/capture_event'
-import { ProjectCreatedTrigger } from './triggers/project_created'
-import { ActionDefinedTrigger } from './triggers/action_defined'
 import { ActionPerformedTrigger } from './triggers/action_performed'
-import { AnnotationCreatedTrigger } from './triggers/annotation_created'
 
 const { version } = require('../package.json') // require() to bypass rootDir restriction
 
@@ -15,10 +12,7 @@ export default {
     authentication,
     beforeRequest: [includeToken],
     triggers: {
-        [ProjectCreatedTrigger.key]: ProjectCreatedTrigger,
-        [ActionDefinedTrigger.key]: ActionDefinedTrigger,
         [ActionPerformedTrigger.key]: ActionPerformedTrigger,
-        [AnnotationCreatedTrigger.key]: AnnotationCreatedTrigger,
     },
     creates: {
         [EventCaptureCreate.key]: EventCaptureCreate,
